80617020705eb81b798502a9cebbcfd4cb1509c3
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-08-19 13:23:09 +0300

    install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:chat_app/constants/theme_constant.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
