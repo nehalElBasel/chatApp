376975263d9f5b9e193412dcbca24a9a4a594ef9
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-08-20 12:13:19 +0300

    install firestore and add message model and class

---
+++ lib/models/message_model.dart
@@ -0,0 +1,10 @@
+class MessageModel {
+  final String message;
+  final String userID;
+  final DateTime date;
+  MessageModel({
+    required this.userID,
+    required this.message,
+    required this.date,
+  });
+}
---
+++ lib/services/firebase_firestore.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CustomFirebaseFireStore {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+}
